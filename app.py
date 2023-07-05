f8fb566ab6c181aeec172f5ddfc5648f556a6eb9
VladSlayer <128102776+VladSlayer@users.noreply.github.com>
2023-07-04 23:37:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,16 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Scott Slayer'
-
+def home():
+    return """
+<center> 
+    <video src="https://comunidadinformatica4-my.sharepoint.com/:v:/g/personal/scottth_comunidadinformatica4_onmicrosoft_com/Ef8a6wDFB9FPjiohZ7-DOUIBi8MdIwoKrZeFSJnvbp8k0w?download=1" width="420" height="240" autoplay controls style="border-radius: 12px;"/> 
+</center> 
+<style>
+    body { 
+        background: antiquewhite;
+    }
+</style>"""
 
 if __name__ == "__main__":
     app.run()
