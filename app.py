97c2158ffc79622ffdf15920cfcb85f6cac64f8f
VladSlayer <128102776+VladSlayer@users.noreply.github.com>
2024-08-06 17:44:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 def home():
     return """
 <center> 
-    <video src="https://comunidadinformatica4-my.sharepoint.com/:v:/g/personal/scottth_comunidadinformatica4_onmicrosoft_com/Ef8a6wDFB9FPjiohZ7-DOUIBi8MdIwoKrZeFSJnvbp8k0w?download=1" width="420" height="240" autoplay controls style="border-radius: 12px;"/> 
+    <video src="" width="420" height="240" autoplay controls style="border-radius: 12px;"/> 
 </center> 
 <style>
     body { 
